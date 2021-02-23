99e2a57e2e7c76ce671c276af25d056631769157
mw18 <67554339+mw18@users.noreply.github.com>
2021-02-23 13:02:42 -0600

    update mongoose connection

--- Develop/server.js
+++ Develop/server.js
@@ -15,12 +15,22 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/budget", {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useCreateIndex: true,
-  useFindAndModify: false
-});
+// mongoose.connect("mongodb://localhost/budget", {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+//   useCreateIndex: true,
+//   useFindAndModify: false
+// });
+
+mongoose.connect(
+  process.env.MONGODB_URI || 'mongodb://localhost/deep-thoughts',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  });
+
 
 // routes
 app.use(require("./routes/api.js"));
