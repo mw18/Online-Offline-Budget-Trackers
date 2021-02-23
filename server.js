73a4728a9b37f4cb17cc32bd2fbdcb32467ccc3a
mw18 <67554339+mw18@users.noreply.github.com>
2021-02-23 12:35:29 -0600

    deploying

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ mongoose.connect("mongodb://localhost/budget", {
 });
 
 // routes
-app.use(require("./routes/api.js"));
+app.use(require("./Main/routes/api.js"));
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
